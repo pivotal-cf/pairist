0bd034e13cac535bfb5d6f67fa4012cdfd7c6259
Luan Santos <cfcluan@gmail.com>
2018-02-04 21:34:58 -0800

    Remove footer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template>
   <v-app id="app">
     <router-view :key="$route.fullPath"/>
-    <Footer/>
   </v-app>
 </template>
 
 <script>
-import Footer from "./components/Footer.vue"
-
 export default {
   name: "App",
-  components: {
-    Footer,
-  },
 }
 </script>
-
-<style>
-#app {
-  display: flex;
-  min-height: 100vh;
-  flex-direction: column;
-}
-#wrapper {
-  flex: 1 0 auto;
-}
-</style>
