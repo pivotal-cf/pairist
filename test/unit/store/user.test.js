8fe1de9d2cbe556b9f5200c21027e0198a48b3f8
Luan Santos <cfcluan@gmail.com>
2018-02-09 09:12:03 -0800

    Backfill tests for user store
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- test/unit/store/user.test.js
+++ test/unit/store/user.test.js
@@ -1,5 +1,4 @@
 import store from "@/store/user"
-import constants from "@/lib/constants"
 
 jest.mock("@/firebase", () => {
   const firebasemock = require("firebase-mock")
@@ -33,6 +32,10 @@ jest.mock("vuexfire", () => {
 })
 
 jest.mock("@/router", () => {
+  global.router = {
+    push: jest.fn(),
+  }
+  return global.router
 })
 
 describe("Users Store", () => {
@@ -61,55 +64,203 @@ describe("Users Store", () => {
 
   describe("actions", () => {
     describe("autoLogin", () => {
-      it("logs in with an email address", () => {
+      it("logs in with a lowercase name", () => {
         const commit = jest.fn()
-        const payload = {
-          email : "foo@foo.com",
-          uid : 1,
-        }
-        store.actions.autoLogin({ commit }, payload)
-        expect(commit).toHaveBeenCalledTimes(1)
-        expect(commit).toHaveBeenCalledWith(
-          "setUser",
-          {
-            uid: payload.uid,
-            name: payload.email,
-          },
-        )
-      })
+          , payload = {
+            email: "FOO@pair.ist",
+            uid: 1,
+          }
 
-      it("logs in with a lowercase email address", () => {
-        const commit = jest.fn()
-        const payload = {
-          email : "FOO@foo.com",
-          uid : 1,
-        }
         store.actions.autoLogin({ commit }, payload)
         expect(commit).toHaveBeenCalledTimes(1)
         expect(commit).toHaveBeenCalledWith(
           "setUser",
           {
-            uid: payload.uid,
-            name: payload.email.toLowerCase(),
+            uid: 1,
+            name: "foo",
           },
         )
       })
 
       it("logs in with a name stripping @pair.ist", () => {
         const commit = jest.fn()
-        const payload = {
-          email : "foo@pair.ist",
-          uid : 1,
-        }
+          , payload = {
+            email: "foo@pair.ist",
+            uid: 1,
+          }
+
         store.actions.autoLogin({ commit }, payload)
         expect(commit).toHaveBeenCalledTimes(1)
         expect(commit).toHaveBeenCalledWith(
           "setUser",
           {
-            uid: payload.uid,
-            name: payload.email.replace("@pair.ist", ""),
+            uid: 1,
+            name: "foo",
           },
         )
+      })
+    })
+
+    describe("signup", () => {
+      it("signs up with given name and password", async () => {
+        const commit = jest.fn()
+          , dispatch = jest.fn()
+          , payload = {
+            name: "MY-NAME",
+            password: "my-password",
+          }
+
+        global.db.ref("/teams/my-name/ownerUID").autoFlush()
+
+        const prom = store.actions.signup({ commit, dispatch }, payload)
+        expect(commit).toHaveBeenCalledTimes(1)
+        expect(commit).toHaveBeenCalledWith("loading", true)
+
+        global.auth.flush()
+
+        const user = await global.auth.getUserByEmail("my-name@pair.ist")
+        expect(user.email).toEqual("my-name@pair.ist")
+        expect(user.password).toEqual("my-password")
+
+        await prom
+
+        expect(commit).toHaveBeenCalledTimes(2)
+        expect(commit).toHaveBeenCalledWith("loading", false)
+
+        const ownerUID = (await global.db.ref("/teams/my-name/ownerUID").once("value")).val()
+        expect(ownerUID).toEqual(user.uid)
+        expect(global.router.push).toHaveBeenCalledWith({ name: "Team", params: { team: "my-name" } })
+      })
+
+      it("notifies user when signup fails", async () => {
+        const commit = jest.fn()
+          , dispatch = jest.fn()
+          , payload = {
+            name: "team1",
+            password: "password1",
+          }
+
+        global.auth.failNext("createUserWithEmailAndPassword", new Error("email address signup fail"))
+
+        const prom = store.actions.signup({ commit, dispatch }, payload)
+        expect(commit).toHaveBeenCalledTimes(1)
+        expect(commit).toHaveBeenCalledWith("loading", true)
+
+        global.auth.flush()
+
+        await prom
+
+        expect(commit).toHaveBeenCalledTimes(3)
+
+        expect(commit).toHaveBeenCalledWith("notify", {
+          message: "name signup fail",
+          color: "error",
+        })
+
+        expect(commit).toHaveBeenCalledWith("loading", false)
+      })
+
+      it("notifies user when creating team fails", async () => {
+        const commit = jest.fn()
+          , dispatch = jest.fn()
+          , payload = {
+            name: "team2",
+            password: "password1",
+          }
+
+        global.db.ref("/teams/team2/ownerUID").failNext("set", new Error("failed creating team"))
+        global.db.ref("/teams/team2/ownerUID").autoFlush()
+
+        const prom = store.actions.signup({ commit, dispatch }, payload)
+        expect(commit).toHaveBeenCalledTimes(1)
+        expect(commit).toHaveBeenCalledWith("loading", true)
+
+        global.auth.flush()
+
+        await prom
+
+        expect(commit).toHaveBeenCalledTimes(3)
+
+        expect(commit).toHaveBeenCalledWith("notify", {
+          message: "You don't have permissions to view this team.",
+          color: "error",
+        })
+
+        expect(dispatch).toHaveBeenCalledTimes(1)
+        expect(dispatch).toHaveBeenCalledWith("logout")
+        expect(commit).toHaveBeenCalledWith("loading", false)
+      })
+    })
+
+    describe("signin", () => {
+      it("signs in with given name and password", async () => {
+        const commit = jest.fn()
+          , dispatch = jest.fn()
+          , payload = {
+            name: "SIGNIN-TEST-NAME",
+            password: "my-password",
+          }
+
+        global.auth.autoFlush()
+
+        const prom = store.actions.signin({ commit, dispatch }, payload)
+        expect(commit).toHaveBeenCalledTimes(1)
+        expect(commit).toHaveBeenCalledWith("loading", true)
+
+        const user = await global.auth.getAuth()
+        expect(user.email).toEqual("signin-test-name@pair.ist")
+
+        await prom
+
+        expect(dispatch).toHaveBeenCalledTimes(1)
+        expect(dispatch).toHaveBeenCalledWith("autoLogin", user)
+
+        expect(global.router.push).toHaveBeenCalledWith({ name: "Team", params: { team: "signin-test-name" } })
+      })
+
+      it("notifies user of failed auth", async () => {
+        const commit = jest.fn()
+          , dispatch = jest.fn()
+          , payload = {
+            name: "SIGNIN-TEST-NAME2",
+            password: "my-password",
+          }
+
+        global.auth.failNext("signInWithEmailAndPassword", new Error("email address signin fail"))
+        global.auth.autoFlush()
+
+        const prom = store.actions.signin({ commit, dispatch }, payload)
+        expect(commit).toHaveBeenCalledTimes(1)
+        expect(commit).toHaveBeenCalledWith("loading", true)
+
+        await prom
+
+        expect(commit).toHaveBeenCalledTimes(3)
+        expect(commit).toHaveBeenCalledWith("loading", false)
+        expect(commit).toHaveBeenCalledWith("notify", {
+          message: "name signin fail",
+          color: "error",
+        })
+      })
+    })
+
+    describe("logout", () => {
+      it("signs out", async () => {
+        const commit = jest.fn()
+
+        global.auth.signInWithEmailAndPassword("me@pairist.ist", "password")
+        global.auth.autoFlush()
+
+        store.actions.logout({ commit })
+
+        const user = await global.auth.getAuth()
+        expect(user).toBeFalsy()
+
+        expect(commit).toHaveBeenCalledTimes(3)
+        expect(commit).toHaveBeenCalledWith("loading", true)
+        expect(commit).toHaveBeenCalledWith("setUser", null)
+        expect(commit).toHaveBeenCalledWith("loading", false)
+        expect(global.router.push).toHaveBeenCalledWith({ name: "Home" })
       })
     })
   })
