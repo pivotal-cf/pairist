be4987a8fd769e4c665868fe1a7dd6e52c339d3a
Luan Santos <cfcluan@gmail.com>
2018-02-11 11:40:59 -0800

    Improve Team.vue tests with proper shallow rendering

--- test/unit/components/Team.test.js
+++ test/unit/components/Team.test.js
@@ -1,4 +1,9 @@
 import { shallow } from "@vue/test-utils"
+import Sidebar from "@/components/team/Sidebar"
+import LaneList from "@/components/team/LaneList"
+import DraggingController from "@/components/team/DraggingController"
+import Toolbar from "@/components/team/Toolbar"
+import Notification from "@/components/Notification"
 import Team from "@/components/team/Team"
 
 const $route = {
@@ -12,8 +17,32 @@ describe("Team", () => {
     shallow(Team, { mocks: { $route } })
   })
 
-  it("lowercases team name in state", () => {
+  it("renders a sidebar", () => {
     const wrapper = shallow(Team, { mocks: { $route } })
-    expect(wrapper.vm.teamName).toEqual("team-name")
+    expect(wrapper.find(Sidebar).exists()).toBeTruthy()
+  })
+
+  it("renders a LaneList", () => {
+    const wrapper = shallow(Team, { mocks: { $route } })
+    expect(wrapper.find(LaneList).exists()).toBeTruthy()
+  })
+
+  it("renders a Notification", () => {
+    const wrapper = shallow(Team, { mocks: { $route } })
+    expect(wrapper.find(Notification).exists()).toBeTruthy()
+  })
+
+  it("renders a DraggingController", () => {
+    const wrapper = shallow(Team, { mocks: { $route } })
+    expect(wrapper.find(DraggingController).exists()).toBeTruthy()
+    expect(wrapper.find(DraggingController).vm.draggables)
+      .toEqual(["person", "track", "role"])
+  })
+
+  it("renders a Toolbar", () => {
+    const wrapper = shallow(Team, { mocks: { $route } })
+    expect(wrapper.find(Toolbar).exists()).toBeTruthy()
+    expect(wrapper.find(Toolbar).vm.teamName)
+      .toEqual("TEAM-NAME")
   })
 })
