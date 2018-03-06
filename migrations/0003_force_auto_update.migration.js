21b0699457d890e7ecdfb57f6ec78f970c259bda
Rob Day Reynolds <rdayreynolds@pivotal.io>
2018-03-06 12:04:18 -0800

    schema version 3 to force updates
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- migrations/0003_force_auto_update.migration.js
+++ migrations/0003_force_auto_update.migration.js
@@ -2,6 +2,6 @@ const Migration = require('./migration')
 
 module.exports = class extends Migration {
   async up () {
-    // pseudo version to create initial schema tracking
+    // here to bump version to 3
   }
 }
