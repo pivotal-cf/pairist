4b588e7888826447dda86d1869331efd101d3af4
Cameron Moreau <cameron.cm6@gmail.com>
2019-06-11 21:43:04 -0700

    Run linter

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   chainWebpack: config => {
     config
-      .plugin("define")
+      .plugin('define')
       .tap(args => {
-        Object.assign(args[0]["process.env"], { VUE_APP_VERSION: Date.now() })
+        Object.assign(args[0]['process.env'], { VUE_APP_VERSION: Date.now() })
         return args
       })
   },
