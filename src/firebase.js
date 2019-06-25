88b8333d053fe283642d924277c41fa661d2597c
Reid Mitchell <rmitchell@pivotal.io>
2019-06-24 18:18:11 -0700

    Use production build of firebase SDK
    Authored-by: Reid Mitchell <rmitchell@pivotal.io>

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
 
 const config = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
