576be831006360b78f8c14722df7f398b1c62e92
Mikael Manukyan <mmanukyan@pivotal.io>
2019-08-30 09:46:14 -0700

    style: change emoji buttons styling

--- src/components/team/EmojiButton.vue
+++ src/components/team/EmojiButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn icon ripple class="emoji-button" @click="handleClick">
+  <v-btn flat ripple class="emoji-button" @click="handleClick">
     <span class="emoji-button__emoji">{{ emojisByName[this.$props.name] }}</span>
     <span v-if="showCount" class="emoji-button__count primary--text">{{ this.$props.count }}</span>
   </v-btn>
@@ -42,12 +42,15 @@ export default {
 <style lang="stylus">
 .emoji-button
   margin: 0
+  padding: 0.25em 0.5em
+  min-width: auto
   font-style: normal
   font-weight: normal
 
   .emoji-button__emoji
     font-size: 1.2em
     width: 1.1em
+    margin-right: 6px
 
   .emoji-button__count
     font-size: 0.9em
