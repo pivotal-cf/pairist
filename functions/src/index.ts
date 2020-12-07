63c2eaff71dadec2ac51694a2065dcd7672a5583
Reid Mitchell <reidmitchell1@gmail.com>
2020-12-07 00:14:17 -0500

    Reimplement API endpoint for current pairs
    Changing the approach to this. The original idea was to have API keys
    stored for each team that could be used as headers in HTTP requests to
    "authenticate". However, there were too many security questions around
    this: although the data returned wasn't really sensitive, these API keys
    were secrets, and there isn't a simple way to store secrets in the
    Firebase DB. After thinking harder, there's a much simpler way to
    achieve a similar thing...
    
    Now, the API endpoint (".../current/<teamId>") is authenticated based on
    your Firebase credentials – you'll need to be logged in as a real user
    and pass an Authorization header with a valid Firebase ID token. For
    teams that want to give programmatic access to certain users, the way to
    do this is just to invite them to the team! This functionality already
    exists and works fine.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,8 @@ import { initializeApp } from 'firebase-admin';
 initializeApp();
 
 export { addTeamMember } from './addTeamMember';
+export { api } from './api';
 export { createTeam } from './createTeam';
-export { current } from './getCurrentPairs';
 export { removeTeamMember } from './removeTeamMember';
 export { saveHistory } from './saveHistory';
 export { updateTeamSettings } from './updateTeamSettings';
