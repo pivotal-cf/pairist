09e53393f74d7843520a4383a65b8fc9ecef0f64
Reid Mitchell <reidmitchell1@gmail.com>
2020-12-12 21:37:55 -0500

    Improve (un)verify-user scripts
    These scripts allow an admin to manually verify or unverify a user by
    email address.

---
+++ scripts/helpers.js
@@ -0,0 +1,37 @@
+const admin = require('firebase-admin');
+
+export async function run(func) {
+  try {
+    await func();
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+export async function initializeFirebase() {
+  const projectId = process.env.PAIRIST_PROJECT_ID;
+  const serviceAccountPath = process.env.PAIRIST_SERVICE_ACCOUNT_PATH;
+
+  if (!projectId) {
+    console.error('PAIRIST_PROJECT_ID is not set.');
+    process.exit(1);
+  }
+
+  if (!serviceAccountPath) {
+    console.error('PAIRIST_SERVICE_ACCOUNT_PATH is not set.');
+    process.exit(1);
+  }
+
+  console.log(`Firebase project is: ${projectId}`);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(require(serviceAccountPath)),
+    databaseURL: `https://${projectId}.firebaseio.com`,
+  });
+
+  return {
+    auth: admin.auth(),
+  };
+}
