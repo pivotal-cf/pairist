3690dbf5f3298594e4643c2dc7bb8ff226bb828f
Josh Aresty <jaresty@gmail.com>
2021-02-13 00:53:39 +0000

    Slight adjustments for shadow display

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -87,15 +87,15 @@ const styles = css`
   @import '../variables.scss';
 
   .contextCount0 {
-    transform: scale(1.05);
-    box-shadow: 0px 25px 10px -15px rgba(0,0,0,0.4);
+    transform: scale(1.1);
+    box-shadow: 0px 24px 15px -15px rgba(0,0,0,0.4);
   }
   .contextCount1 {
-    box-shadow: 0px 20px 8px -10px rgba(0,0,0,0.4);
+    box-shadow: 0px 16px 10px -6px rgba(0,0,0,0.4);
   }
   .contextCount2 {
     transform: scale(0.95);
-    box-shadow: 0px 10px 5px -5px rgba(0,0,0,0.4);
+    box-shadow: 0px 8px 5px -3px rgba(0,0,0,0.4);
   }
   .contextCount3 {
     transform: scale(0.9);
